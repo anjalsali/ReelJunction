25ce5719afa172cc0bed1ac202df6afc360d42d5
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2024-01-03 16:48:30 +0000

    most of the page is functional created a div for the youtube player having issues with cookies for youtube

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,70 @@
-$(document).ready(function() {
-    $('#searchBtn').on('click', function() {
-        var movieTitle = $('#movieInput').val();
-        if (movieTitle.trim() !== '') {
-            searchMovieInfo(movieTitle);
-        } else {
-            alert('Please enter a movie title');
+const API_KEY = 'AIzaSyAcyFaQuKnjyDgqIrS7KiSfweuz8aRZ1wI'; // Replace with your actual YouTube API key
+const videoPlayer = document.getElementById('youtube-player');
+
+function playYouTubeVideo(videoId) {
+  if (typeof YT !== 'undefined' && YT.loaded) {
+    // If the YouTube API is already loaded, create the player immediately
+    createYouTubePlayer(videoId);
+  } else {
+    // If the YouTube API is not loaded, dynamically load it
+    loadYouTubeAPI(() => createYouTubePlayer(videoId));
+  }
+}
+
+function loadYouTubeAPI(callback) {
+  const tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Callback function to execute when the YouTube API is ready
+  window.onYouTubeIframeAPIReady = function () {
+    callback();
+  };
+}
+
+function createYouTubePlayer(videoId) {
+  new YT.Player(videoPlayer, {
+    height: '360',
+    width: '640',
+    videoId: videoId,
+    playerVars: {
+      'autoplay': 1,
+    },
+  });
+}
+
+$("#find-movie").on("click", function (event) {
+  event.preventDefault();
+
+  var movie = $("#movie-input").val();
+  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.Response === "True") {
+        $("#movie-view").empty();
+
+        var title = $("<h2>").text(data.Title);
+        var year = $("<p>").text("Year: " + data.Year);
+        var genre = $("<p>").text("Genre: " + data.Genre);
+        var plot = $("<p>").text("Plot: " + data.Plot);
+
+        $("#movie-view").append(title, year, genre, plot);
+
+        // Assuming you have a property like 'youtubeVideoId' in your OMDB data
+        if (data.youtubeVideoId) {
+          // Play YouTube video
+          playYouTubeVideo(data.youtubeVideoId);
         }
+      } else {
+        $("#movie-view").text("Movie not found!");
+      }
+    })
+    .catch(function (error) {
+      console.log("Error fetching data: ", error);
     });
-
-    function searchMovieInfo(movieTitle) {
-        // Call OMDB API to get movie information
-        $.ajax({
-            url: 'http://www.omdbapi.com/',
-            type: 'GET',
-            data: {
-                apikey: 'YOUR_OMDB_API_KEY',
-                t: movieTitle
-            },
-            success: function(movieData) {
-                displayMovieInfo(movieData);
-            },
-            error: function() {
-                alert('Error fetching movie information');
-            }
-        });
-
-        // Call YouTube API to search for relevant videos
-        $.ajax({
-            url: 'https://www.googleapis.com/youtube/v3/search',
-            type: 'GET',
-            data: {
-                key: 'YOUR_YOUTUBE_API_KEY',
-                q: movieTitle + ' official trailer',
-                part: 'snippet',
-                maxResults: 1
-            },
-            success: function(videoData) {
-                displayVideo(videoData);
-            },
-            error: function() {
-                alert('Error fetching video information');
-            }
-        });
-    }
-
-    function displayMovieInfo(movieData) {
-        var infoHtml = '<h2>' + movieData.Title + '</h2>' +
-                       '<p><strong>Rating:</strong> ' + movieData.imdbRating + '</p>' +
-                       '<p><strong>Cast:</strong> ' + movieData.Actors + '</p>' +
-                       '<p><strong>Director:</strong> ' + movieData.Director + '</p>' +
-                       '<p><strong>Release Date:</strong> ' + movieData.Released + '</p>' +
-                       '<p><strong>Runtime:</strong> ' + movieData.Runtime + '</p>';
-        $('#movieInfo').html(infoHtml);
-    }
-
-    function displayVideo(videoData) {
-        if (videoData.items.length > 0) {
-            var videoId = videoData.items[0].id.videoId;
-            var videoHtml = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
-            $('#videoContainer').html(videoHtml);
-        } else {
-            $('#videoContainer').html('<p>No videos found</p>');
-        }
-    }
 });
